b95fa95295929bf775c40d9b54644bff9988ca0d
YoungJin <caribjin@gmail.com>
2021-04-16 22:51:18 +0900

    added image input widget.

--- lib/pages/add_place_page.dart
+++ lib/pages/add_place_page.dart
@@ -1,21 +1,32 @@
+import 'package:daily_shop/widgets/image_input.dart';
 import 'package:flutter/material.dart';
 
 class AddPlacePage extends StatelessWidget {
+  final TextEditingController _titleController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text('Add Place')),
       body: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
         crossAxisAlignment: CrossAxisAlignment.stretch,
         children: [
-          Column(
-            children: [
-              TextField(),
-              TextField(),
-              TextField(),
-              TextField(),
-            ],
+          Expanded(
+            child: SingleChildScrollView(
+              child: Padding(
+                padding: const EdgeInsets.all(10.0),
+                child: Column(
+                  children: [
+                    TextField(
+                      decoration: InputDecoration(labelText: 'Title'),
+                      controller: _titleController,
+                    ),
+                    SizedBox(height: 10),
+                    ImageInput(),
+                  ],
+                ),
+              ),
+            ),
           ),
           ElevatedButton.icon(
             icon: Icon(Icons.add),
@@ -24,9 +35,7 @@ class AddPlacePage extends StatelessWidget {
               elevation: 0,
               tapTargetSize: MaterialTapTargetSize.shrinkWrap,
             ),
-            onPressed: () {
-
-            },
+            onPressed: () {},
           ),
         ],
       ),
