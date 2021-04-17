18d5b72b7c13a044b6f654c3a7b2d9178369d34d
YoungJin <caribjin@gmail.com>
2021-04-17 19:24:08 +0900

    added Camera photo taking feature.

--- lib/widgets/image_input.dart
+++ lib/widgets/image_input.dart
@@ -1,6 +1,7 @@
 import 'dart:io';
 
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
 
 class ImageInput extends StatefulWidget {
   @override
@@ -9,6 +10,20 @@ class ImageInput extends StatefulWidget {
 
 class _ImageInputState extends State<ImageInput> {
   File? _storedImage;
+  final picker = ImagePicker();
+
+  Future<void> _takePicture() async {
+    final pickedFile = await picker.getImage(
+      source: ImageSource.camera,
+      maxWidth: 600,
+    );
+
+    setState(() {
+      if (pickedFile != null) {
+        _storedImage = File(pickedFile.path);
+      }
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -37,7 +52,7 @@ class _ImageInputState extends State<ImageInput> {
         TextButton.icon(
           icon: Icon(Icons.camera),
           label: Text('Take Picture'),
-          onPressed: () {},
+          onPressed: _takePicture,
         ),
       ],
     );
