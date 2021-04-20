a11b054da0b06c1ef63d6fe2897746cb92d3c3e9
YoungJin Lim <caribjin@gmail.com>
2021-04-20 21:52:33 +0900

    added places list page.

--- lib/pages/places_list_page.dart
+++ lib/pages/places_list_page.dart
@@ -1,11 +1,32 @@
+import 'package:daily_shop/providers/places.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class PlacesListPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-      ],
+    return Consumer<Places>(
+      builder: (context, placesData, child) {
+        if (placesData.items.length <= 0) {
+          return child!;
+        } else {
+          return ListView.builder(
+            itemBuilder: (context, index) {
+              return ListTile(
+                  leading: CircleAvatar(
+                    backgroundImage: FileImage(placesData.items[index].image),
+                  ),
+                  title: Text(placesData.items[index].title),
+                  subtitle: Text(placesData.items[index].location.address),
+                  onTap: () {});
+            },
+            itemCount: placesData.items.length,
+          );
+        }
+      },
+      child: Center(
+        child: Text('Got no places yet'),
+      ),
     );
   }
 }
